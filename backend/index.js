e8f61713b63c13fcb412cdae467975ba25edf327
Deepak Sharma <deepakmyside@gmail.com>
2026-04-11 19:47:23 +0530

    feat: add socketHandler with real-time collaboration

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 require("dotenv").config()
 const app = express()
+const http = require('http')
+const { Server} = require('socket.io')
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
 const connectDB = require("./config/db")
+const socketHandler = require('./socket/socketHandler')
+
 
 app.use(express.json())
 
@@ -11,11 +22,12 @@ const noteRouter = require("./routes/noteRoutes")
 app.use("/api/auth", authRouter)
 app.use("/api/notes", noteRouter)
 
+socketHandler(io)
 connectDB()
 app.get("/",(req,res) => {
     res.send("Backend server is running now")
 })
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server running on port 3000')
 })
---
+++ backend/socket/socketHandler.js
@@ -0,0 +1,32 @@
+const socketHandler = (io) => {
+    io.on('connection', (socket) => {
+        console.log('User connected:' , socket.id)
+      
+        // User join room
+        socket.on('join-room', (roomId) => {
+            socket.join(roomId)
+            console.log(`User ${socket.id} joined room ${roomId}`)
+
+            //  Telling all users in room that new user joined
+            socket.to(roomId).emit('user-joined', socket.id)
+        })
+
+        //  Send everyone that user is typing
+        socket.on('note-change', ({ roomId, content, title})  => {
+            console.log('note-change received', roomId, content)
+            socket.to(roomId).emit('note-change', { content, title })
+        })
+        // User typing indicator
+        socket.on('user-typing', ({ roomId, userName}) => {
+            socket.to(roomId).emit('user-typing', userName)
+        })
+
+        //  User is disconnect
+        socket.on('disconnect', () => {
+            console.log('User disconnected:', socket.id)
+        }) 
+      
+    })
+}
+
+module.exports = socketHandler
