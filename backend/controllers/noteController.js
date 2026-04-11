cfa812f16e562f79ab94aa5165d1729b0ca23233
Deepak Sharma <deepakmyside@gmail.com>
2026-04-11 11:49:42 +0530

    feat: add note controller

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,83 @@
+const Note = require('../models/Note')
+
+
+const createNote = async (req, res) => {
+       try {
+        const { title, content} = req.body
+        const roomId = `room_${Date.now()}_${Math.floor(Math.random() * 10000)}`
+
+        const note = await Note.create({
+             title,
+             content,
+             owner:req.user.id,
+             collaborators: [],
+             roomId
+            })
+         res.status(201).json({note})
+
+    } catch (error) {
+         console.log(error)
+         res.status(500).json({ message: 'Server error', error})
+        }
+}
+
+
+const getAllNotes = async (req, res) => {
+    try{
+
+        const note = await Note.find({
+            $or: [
+                { owner: req.user.id},
+                {collaborators: req.user.id}
+            ]
+        })
+
+        res.status(200).json(note)
+    
+    }catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error", error})
+    }
+}
+
+const getNoteById = async (req, res) => {
+    try{
+     
+        const note = await Note.findOne({ roomId: req.params.roomId})
+        if(!note) {
+            return res.status(404).json({ message:'Note not found or deleted '})
+        }
+
+        res.status(200).json(note)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Server error', error})
+    }
+}
+
+const updateNote = async (req, res) => {
+    try {
+        const note = await Note.findOne({ roomId: req.params.roomId})
+        if(!update) {
+            return res.status(404).json({ message: 'Note not found'})
+        }
+// comparing ids
+// converting from object mongoid to string (jwt se compare krna h aur woh string h )
+        const isOwner = note.owner.toString() === req.user.user.id
+        const isCollaborator = note.collaborators.map(id.toString()).includes(req.user.id)
+
+        if(!isOwner && !isCollaborator) {
+            return res.status(403).json({ message: 'Access denied, Invalid Credentials'})
+        }
+        note.title = req.body.title || note.title
+        note.content = req.body.content || note.content
+
+        await note.save()
+
+        res.status(200).json(note)
+
+    } catch (error) {
+        console.log(error)
+        res.stataus(500).json({message: "Server error", error})
+    }
+}
