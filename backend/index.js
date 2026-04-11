8266855f104fbf1073f818bf9c4828da41e3f14f
Deepak Sharma <deepakmyside@gmail.com>
2026-04-11 14:26:42 +0530

    fix: typing mistakes

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,15 @@ const connectDB = require("./config/db")
 app.use(express.json())
 
 const authRouter = require("./routes/authRoutes")
+const noteRouter = require("./routes/noteRoutes")
+
+app.use("/api/auth", authRouter)
+app.use("/api/notes", noteRouter)
+
 connectDB()
 app.get("/",(req,res) => {
     res.send("Backend server is running now")
 })
-
-app.use("/api/auth", authRouter)
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
