c145fc7bf43a06343ddd092c530d25ba5da531f7
Deepak Sharma <deepakmyside@gmail.com>
2026-04-11 21:52:59 +0530

    feat: add React Router with protected routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Editor from './pages/Editor'
+
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token')
+  return token ? children : <Navigate to="/" />
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+           <Routes>
+                 <Route path="/" element={<Login/>} />
+                 <Route path="/dashboard" element={
+                  <PrivateRoute>
+                       <Dashboard />
+                  </PrivateRoute>
+                 }  />
+                 <Route path="/editor/:roomId" element={
+                  <PrivateRoute>
+                     <Editor/>
+                  </PrivateRoute>
+                 }  />
+           </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
